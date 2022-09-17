97c290e5aa9be3a3ebc7bb095137d2f472f7c57f
Dirceu Silvestre <dirceu_silvestre@hotmail.com>
2022-09-17 11:30:16 -0300

    notebooks de analises de dados

---
+++ Slide.py
@@ -0,0 +1,177 @@
+'''Modelo para Padronização dos Materiais de Apoio das Linguagens
+
+
+       Arquivo  "Slide.linguagem"
+
+
+       Compilar
+
+Linux:	 
+    
+    py ou python3 arquivo.py
+
+Windows:    
+
+    py ou python3 arquivo.py
+
+
+0  -  Sobre a Linguagem		(breve historia, motivacao, paradigma, qualidades)
+
+1  -  Local de cada coisa no código	(como é a estrutura básica da codificação nessa linguagem)
+
+    Estruturado
+
+        bibliotecas
+        constantes
+        variaveis globais
+
+        funcoes
+        main
+
+    Orientado Objeto
+
+        bibliotecas
+        classes pai
+        classes filho
+        main
+
+
+2  -  Comentarios			(o que usam para fazer comentários no código)
+
+    '''  '''    comentário de texto
+    """  """    comentario de texto
+    #           comentário de linha
+
+
+3  -  Atribuição			(simbolo usado para atribuicao dos dados a uma variavel e como atribuir)
+
+    =
+
+
+4  -  Tipos de Dados			(primitivos, nao primitivos, comum as linguagens e especificos dela)
+
+    type()
+
+    Primitivos
+
+        int             1, 2, 3             
+        float           1.0, 3.333
+        str             letra e texto
+        bool            True ou False
+
+    Nao Primitivo
+
+        lista
+        tupla
+        dicionario
+
+
+5  -  Modificadores			(java e outras linguagens que tenham)
+
+6  -  Variavel e Constante		(local, global, parametro, constante, classe, atributo, metodo)
+
+    local           dentro da função
+    global          fora da função, antes do main, logo após as bibliotecas
+
+    variavel
+    CONSTANTE
+
+
+7  -  Declaração			(como declaram todos os tipos de dados)
+
+    variavel = modificador(input())             modificador pode ser (int, str, float, bool)
+    
+
+8  -  Operadores (matematicos, logicos, comparacao)
+
+    Matematicos
+
+        +           soma
+        -           subtração
+        *           multiplicação
+        /           divisão
+        //          divisão inteira
+        %           módulo ou resto da divisão
+        **          potenciação
+
+    Atribuição
+
+        +=          soma e atribui
+        -=          subtrai e atribui
+        *=          multiplica e atribui
+        /=          divide e atribui
+        %=          calcula o resto e atribui
+
+    Comparação
+
+        >           maior que
+        <           menor que
+        ==          igual
+        !=          diferente
+        >=          maior ou igual
+        <=          menor ou igual
+
+    Lógico
+
+        and         e
+        or          ou
+        not         negação
+
+    Identidade
+
+        is          retorna True se forem os mesmos objetos
+        is not      retorna True se nao forem os mesmos objetos
+
+    Associação 
+
+        in          True se estiver dentro
+        not in      True se não estiver dentro
+
+
+9  -  Impressao			(formas de imprimir em cada local, como saida padrao no terminal ou arquivo)
+
+    print()
+
+
+10  -  Escaneamento			(formas de coletar o dado e os locais, como teclado ou arquivo)
+
+    variavel = input()
+
+
+11  -  Estruturas (condicionais, repeticao)
+
+    Controle
+
+        if:
+        elif:
+        else:
+
+    Repeticao
+
+        for i in range:
+        for item in lista:
+        for letra in palavra:
+        for chave,valor in dicionario.items()
+
+        for 
+        else
+
+        while condicao:
+
+        while
+        else
+
+
+12  -  Bibliotecas			(bibliotecas mais usadas da linguagem)
+
+13  -  Função e Parâmetro		(como se declara uma função e seus parametros)
+
+14  -  Ponteiro			(como trabalhar com ponteiros na linguagem)
+
+15  -  Alocação Dinâmica		(alocar e desalocar memória)
+
+16  -  Leitura e Escrita em arquivo	(toda a forma de se trabalhar com arquivo exceto impressao e escaneamento)
+
+17  -  Topicos Avançados (macro, construir biblioteca, compilação, api)
+'''
+

1777e5e954a1fb0031afd26b2c0942a3afc2fab1
Dirceu Silvestre <dirceu_silvestre@hotmail.com>
2022-09-17 23:14:08 -0300

    continuando aprendizado em python

--- Slide.py
+++ Slide.py
@@ -32,6 +32,7 @@ Windows:
 
         bibliotecas
         classes pai
+        {atributos, metodos, construtor}
         classes filho
         main
 
@@ -50,8 +51,6 @@ Windows:
 
 4  -  Tipos de Dados			(primitivos, nao primitivos, comum as linguagens e especificos dela)
 
-    type()
-
     Primitivos
 
         int             1, 2, 3             
@@ -64,6 +63,7 @@ Windows:
         lista
         tupla
         dicionario
+        
 
 
 5  -  Modificadores			(java e outras linguagens que tenham)
@@ -76,11 +76,54 @@ Windows:
     variavel
     CONSTANTE
 
+    self            é como o this
+
 
 7  -  Declaração			(como declaram todos os tipos de dados)
 
     variavel = modificador(input())             modificador pode ser (int, str, float, bool)
-    
+                                                sem o modificador a entrada é string
+
+    lista = []                  declara lista vazia
+    lista = list()              converte alguma estrutura em lista
+    lista = [10, [1, 2, 3]]     lista de listas
+
+    tupla = ()                  tupla é imutavel
+    tupla = tuple()
+    tupla = item, item, item
+
+    dicionario = {}
+    dicionario = dict()
+    dicionario = {'chave': 'valor'}
+
+    Lista
+
+        lista[indice]
+        lista[linha][coluna]
+        lista[inicio:final - 1:passo]       fatiamento
+
+    Tupla
+
+        tupla[indice]
+        tupla[fati:a:mento]
+
+    Dicionario
+
+        dicionario[chave]
+        dicionario[nova chave] = item       adiciona nova chave e item nessa chave
+        chave in dicionario                 retorna True se tiver
+
+    def funcao(arg, *args):
+        for argumentos in arg:
+            print(argumentos)
+
+    class Classe:
+
+        def __init__(self, atributo: tipo):         construtor
+            self.atributo = atributo    
+
+        def metodo(self, parametro: tipo):
+
 
 8  -  Operadores (matematicos, logicos, comparacao)
 
@@ -166,12 +209,64 @@ Windows:
 
 13  -  Função e Parâmetro		(como se declara uma função e seus parametros)
 
+    Parametros
+
+        *args           usado para receber uma lista de argumentos nao nomeados
+        **kwargs        recebe argumentos nomeados, funcionando como um dicionario, onde os nomes são as chaves e os valores valores
+
+    type()              retorna tipo
+    len()               retorna tamanho
+    range()             retorna numeros até o final - 1
+    sum()               retorna a soma de todos os itens
+    max()               retorna o maior valor da lista
+    min()               retorna o menor valor da lista
+
+    def funcao(parametros):
+        return
+
+        parametro='alguma coisa'        entende que se nao passar nada, o 'alguma coisa' sera o valor padrao
+
+
 14  -  Ponteiro			(como trabalhar com ponteiros na linguagem)
 
 15  -  Alocação Dinâmica		(alocar e desalocar memória)
 
 16  -  Leitura e Escrita em arquivo	(toda a forma de se trabalhar com arquivo exceto impressao e escaneamento)
 
-17  -  Topicos Avançados (macro, construir biblioteca, compilação, api)
-'''
+    with open() as variavel:
+    variavel = open(nome_do_arquivo, modo)
 
+        modo                é o que queremos fazer com o arquivo, ler escrever etc
+
+            r               reading, leitura
+            w               write, escrita (cuidado pois sobrescreve o arquivo)
+            a               de append, adiciona ao final do arquivo
+            b               para trabalhar no modo binario
+
+    variavel.write()
+    variavel.read()
+    variavel.close()
+
+
+17  -  Topicos Avançados (macro, construir biblioteca, compilação, api)
+
+    Metodos
+    
+        Lista
+
+            .append()                   adiciona item ao final
+            .insert(indice, item)       adiciona o item no indice especificado
+            .extend()                   adiciona os itens ao final, concatena
+            .pop()                      remove o ultimo item ou item no indice especificado
+            .remove()                   remove o item especificado no menor indice, na primeira aparição
+            .index()                    retorna o index do item especificado
+            .sort()                     ordena de forma crescente os itens
+            .sort(reverse=True)         ordena de forma decrescente
+
+        Tupla
+
+            .index()
+
+
+    
+'''
